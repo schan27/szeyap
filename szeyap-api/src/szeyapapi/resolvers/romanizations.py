12b6e0806207b0cce0ea64565ea26eaef23f8fc9
Sophia Chan <ceasings@gmail.com>
2025-08-20 17:30:36 -0400

    use UNK as default romanization

--- szeyap-api/src/szeyapapi/resolvers/romanizations.py
+++ szeyap-api/src/szeyapapi/resolvers/romanizations.py
@@ -2,7 +2,7 @@ from ..utils.enums import LanguageFormats as Lang
 from ..translation_logic.penyim import Penyim
 
 
-def get(phrase: str, src_lang: str):
+def get(phrase: str, src_lang: str = "UNK"):
     # construct Jyutping Object using phrase as sample
     penyim = Penyim(phrase, Lang[src_lang])
 
