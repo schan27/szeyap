71c3beb11c87a04eb0c967359f3a1c7e97f62c16
saltyseadawg <sabrinayu7@gmail.com>
2025-09-09 21:59:02 -0400

    chore: lint test file

--- szeyap-api/tests/test_translator.py
+++ szeyap-api/tests/test_translator.py
@@ -1,10 +1,11 @@
 import unittest
-from pathlib import Path 
+from pathlib import Path
 from unittest.mock import patch
 
 from szeyapapi.translation_logic.question import TranslationQuestion
 from szeyapapi.translation_logic.translator import Translator
 from szeyapapi.utils.enums import LanguageFormats as Lang
+
 
 class TestSearch(unittest.TestCase):
     # see full test error
@@ -17,14 +18,14 @@ class TestSearch(unittest.TestCase):
     @patch("szeyapapi.config.GENE_CHIN_DICTIONARY_PATH", dict_path)
     @patch("szeyapapi.config.PROJ_ROOT", current_dir)
     def setUp(self):
-        
         # not exactly sure why this works yet but smthng to do with import order:
         # https://www.reddit.com/r/learnpython/comments/lriqrq/how_to_mock_class_constants_imported_into_an/
         from szeyapapi.dictionaries.genechin_dictionary import GeneChinDictionary
-        self.test_dict = GeneChinDictionary("Test Dictionary", "www.testurl.com")        
+
+        self.test_dict = GeneChinDictionary("Test Dictionary", "www.testurl.com")
         self.test_dict.load_dictionary()
         self.trans = Translator("Test Dictionary Translator", self.test_dict)
-        
+
     def test_ask(self):
         question = TranslationQuestion("cats")
         res = self.trans.ask(question, 1).as_api_resp()
@@ -33,7 +34,7 @@ class TestSearch(unittest.TestCase):
             "detected_language": Lang.EN,
             "metadata": {
                 "dictionary_name": "Test Dictionary",
-                "dictionary_url": "www.testurl.com"
+                "dictionary_url": "www.testurl.com",
             },
             "translations": [
                 {
@@ -41,25 +42,27 @@ class TestSearch(unittest.TestCase):
                     "chinese": {
                         "penyim": [
                             {
-                                Lang.DJ: 'mao-',
-                                Lang.GC: 'māo',
-                                Lang.HSR: 'mau55',
-                                Lang.JW: 'mao2',
-                                Lang.SL: 'mau55'
+                                Lang.DJ: "mao-",
+                                Lang.GC: "māo",
+                                Lang.HSR: "mau55",
+                                Lang.JW: "mao2",
+                                Lang.SL: "mau55",
                             }
                         ],
                         "traditional": ["\u8c93"],
                         "simplified": ["\u732b"],
-                    }
+                    },
                 }
-            ]
+            ],
         }
         self.assertEqual(expected_res, res)
 
     def test_english_search(self):
         question = TranslationQuestion("cats")
-        matches = list(self.trans._search_dictionary(question.query, "DEFN", full_match=True))
-        
+        matches = list(
+            self.trans._search_dictionary(question.query, "DEFN", full_match=True)
+        )
+
         expected_matches = 2
         self.assertEqual(expected_matches, len(matches))
 
