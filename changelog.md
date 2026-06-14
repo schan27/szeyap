da16036ffd2d648b04235795c296124f928b2136
Maria Julia Curro <55163943+Maria-Curro-99@users.noreply.github.com>
2026-06-13 21:18:31 -0400

    changelog and version number

---
+++ changelog.md
@@ -0,0 +1,18 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on Keep a Changelog.
+
+## [Unrelased] -2026-06-10
+
+### Added
+
+* Added project changelog.
+* Added version tracking and release documentation process.
+
+### Changed
+
+* Standardized project release management.
+
+
--- szeyap-web-client/src/components/Footer.js
+++ szeyap-web-client/src/components/Footer.js
@@ -1,17 +1,23 @@
+import versionData from "../../public/version.json";
+
 export default function Footer() {
   return (
     <footer className="w-full py-6 mt-auto border-t border-gray-200">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-center flex flex-col gap-2">
         <div>
           <span className="text-sm text-gray-600">License: </span>
-          <a 
-            href="https://creativecommons.org/licenses/by-nc-sa/4.0/" 
-            target="_blank" 
+          <a
+            href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+            target="_blank"
             rel="noopener noreferrer"
             className="text-sm text-gray-600 hover:text-gray-800 hover:underline"
           >
             Attribution-NonCommercial-ShareAlike 4.0 International
           </a>
+        </div>
+
+        <div className="text-xs text-gray-500">
+          Version {versionData.version}
         </div>
       </div>
     </footer>
