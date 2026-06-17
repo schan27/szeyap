d7d980f226441a61cdcc3fb91d495cb47f294e38
Sophia Chan <ceasings@gmail.com>
2026-06-17 12:29:13 -0400

    update test to use PenyimFormats

--- szeyap-api/tests/test_penyim.py
+++ szeyap-api/tests/test_penyim.py
@@ -2,180 +2,186 @@
 from unicodedata import normalize
 
 from szeyapapi.translation_logic.penyim import Penyim
-from szeyapapi.utils.enums import PenyimFormats as Lang
+from szeyapapi.utils.enums import PenyimFormats
 from unidecode import unidecode
 
 
 def test_gc_no_initial():
     test = "ō"
-    result = Penyim(test, Lang.GC)
-    assert normalize("NFD", result.formats[0][Lang.GC]) == normalize("NFD", "ō")
+    result = Penyim(test, PenyimFormats.GC)
+    assert normalize("NFD", result.formats[0][PenyimFormats.GC]) == normalize(
+        "NFD", "ō"
+    )
 
 
 def test_gc_syllable():
     test = "dā"
-    result = Penyim(test, Lang.GC)
-    assert normalize("NFD", result.formats[0][Lang.GC]) == normalize("NFD", "dā")
+    result = Penyim(test, PenyimFormats.GC)
+    assert normalize("NFD", result.formats[0][PenyimFormats.GC]) == normalize(
+        "NFD", "dā"
+    )
 
 
 def test_sc_phrase():
     test = "vi32 saŋ33 dzi55"
-    result = Penyim(test, Lang.SL)
+    result = Penyim(test, PenyimFormats.SL)
     expected_result = ["vi32", "saŋ33", "dzi55"]
     for i, word_formats in enumerate(result.formats):
-        assert unidecode(word_formats[Lang.SL]) == unidecode(expected_result[i])
+        assert unidecode(word_formats[PenyimFormats.SL]) == unidecode(
+            expected_result[i]
+        )
 
 
 def test_basic_no_tone():
     test = "ni"
-    result = Penyim(test, Lang.UNK)
-    assert unidecode(result.formats[0][Lang.HSR]) == unidecode("n(e)i")
-    assert unidecode(result.formats[0][Lang.GC]) == unidecode("ni")
-    assert unidecode(result.formats[0][Lang.SL]) == unidecode("ni")
-    assert unidecode(result.formats[0][Lang.DJ]) == unidecode("n(e)i")
-    assert unidecode(result.formats[0][Lang.JW]) == unidecode("n(e)i")
+    result = Penyim(test, PenyimFormats.UNK)
+    assert unidecode(result.formats[0][PenyimFormats.HSR]) == unidecode("n(e)i")
+    assert unidecode(result.formats[0][PenyimFormats.GC]) == unidecode("ni")
+    assert unidecode(result.formats[0][PenyimFormats.SL]) == unidecode("ni")
+    assert unidecode(result.formats[0][PenyimFormats.DJ]) == unidecode("n(e)i")
+    assert unidecode(result.formats[0][PenyimFormats.JW]) == unidecode("n(e)i")
 
 
 def test_no_space():
     test = "neihau"
 
-    result = Penyim(test, Lang.UNK)
-    assert unidecode(result.formats[0][Lang.HSR]) == unidecode("n(e)i")
-    assert unidecode(result.formats[1][Lang.HSR]) == unidecode("hau")
+    result = Penyim(test, PenyimFormats.UNK)
+    assert unidecode(result.formats[0][PenyimFormats.HSR]) == unidecode("n(e)i")
+    assert unidecode(result.formats[1][PenyimFormats.HSR]) == unidecode("hau")
 
 
 def test_cei():
     test = "cei ao"
-    result = Penyim(test, Lang.UNK)
-    assert unidecode(result.formats[0][Lang.HSR]) == unidecode("tsi")
-    assert unidecode(result.formats[1][Lang.HSR]) == unidecode("au")
+    result = Penyim(test, PenyimFormats.UNK)
+    assert unidecode(result.formats[0][PenyimFormats.HSR]) == unidecode("tsi")
+    assert unidecode(result.formats[1][PenyimFormats.HSR]) == unidecode("au")
 
 
 def test_lh_reversal():
     test = "hlam1"
 
-    result = Penyim(test, Lang.UNK)
-    assert unidecode(result.formats[0][Lang.HSR]) == unidecode("lham33")
+    result = Penyim(test, PenyimFormats.UNK)
+    assert unidecode(result.formats[0][PenyimFormats.HSR]) == unidecode("lham33")
 
 
 def test_mix_romanization_tone():
     test = "xel33"
 
-    result = Penyim(test, Lang.UNK)
-    assert unidecode(result.formats[0][Lang.HSR]) == unidecode("lhiau33")
+    result = Penyim(test, PenyimFormats.UNK)
+    assert unidecode(result.formats[0][PenyimFormats.HSR]) == unidecode("lhiau33")
 
 
 def test_diacritic_markers():
     test1 = "chïäo"
     test2 = "chïao33"
 
-    result1 = Penyim(test1, Lang.UNK)
-    result2 = Penyim(test2, Lang.UNK)
+    result1 = Penyim(test1, PenyimFormats.UNK)
+    result2 = Penyim(test2, PenyimFormats.UNK)
 
-    assert unidecode(result1.formats[0][Lang.HSR]) == unidecode("tsi33")
-    assert unidecode(result1.formats[1][Lang.HSR]) == unidecode("au33")
-    assert unidecode(result2.formats[0][Lang.HSR]) == unidecode("tsi33")
-    assert unidecode(result2.formats[1][Lang.HSR]) == unidecode("au33")
+    assert unidecode(result1.formats[0][PenyimFormats.HSR]) == unidecode("tsi33")
+    assert unidecode(result1.formats[1][PenyimFormats.HSR]) == unidecode("au33")
+    assert unidecode(result2.formats[0][PenyimFormats.HSR]) == unidecode("tsi33")
+    assert unidecode(result2.formats[1][PenyimFormats.HSR]) == unidecode("au33")
 
 
 # def test_double_tone():
 #     test = "hao-’"
-#     result = Penyim(test, Lang.UNK)
-#     assert unidecode(result.formats[0][Lang.HSR]) == unidecode("hau553")
+#     result = Penyim(test, PenyimFormats.UNK)
+#     assert unidecode(result.formats[0][PenyimFormats.HSR]) == unidecode("hau553")
 
 
 def test_bracket_input():
     test1 = "d(e)i"
     test2 = "c(h)a"
-    result1 = Penyim(test1, Lang.UNK)
-    result2 = Penyim(test2, Lang.UNK)
-    assert unidecode(result1.formats[0][Lang.HSR]) == unidecode("d(e)i")
-    assert unidecode(result2.formats[0][Lang.HSR]) == unidecode("tsa")
+    result1 = Penyim(test1, PenyimFormats.UNK)
+    result2 = Penyim(test2, PenyimFormats.UNK)
+    assert unidecode(result1.formats[0][PenyimFormats.HSR]) == unidecode("d(e)i")
+    assert unidecode(result2.formats[0][PenyimFormats.HSR]) == unidecode("tsa")
 
 
 def test_unknown_term():
     test = "nai5fuk215"
-    result = Penyim(test, Lang.UNK)
-    assert unidecode(result.formats[0][Lang.HSR]) == unidecode("nai21")
-    assert unidecode(result.formats[1][Lang.HSR]) == unidecode("fuk215")
+    result = Penyim(test, PenyimFormats.UNK)
+    assert unidecode(result.formats[0][PenyimFormats.HSR]) == unidecode("nai21")
+    assert unidecode(result.formats[1][PenyimFormats.HSR]) == unidecode("fuk215")
 
 
 def test_ng_reversal():
     test = "gno"
-    result = Penyim(test, Lang.UNK)
-    assert unidecode(result.formats[0][Lang.HSR]) == unidecode("ngo")
+    result = Penyim(test, PenyimFormats.UNK)
+    assert unidecode(result.formats[0][PenyimFormats.HSR]) == unidecode("ngo")
 
 
 def test_variants():
     test1 = "sha"
-    result1 = Penyim(test1, Lang.UNK)
-    assert unidecode(result1.formats[0][Lang.HSR]) == unidecode("sa")
+    result1 = Penyim(test1, PenyimFormats.UNK)
+    assert unidecode(result1.formats[0][PenyimFormats.HSR]) == unidecode("sa")
 
     test2 = "chee"
-    result2 = Penyim(test2, Lang.UNK)
-    assert unidecode(result2.formats[0][Lang.HSR]) == unidecode("tsi")
+    result2 = Penyim(test2, PenyimFormats.UNK)
+    assert unidecode(result2.formats[0][PenyimFormats.HSR]) == unidecode("tsi")
 
     test3 = "wa"
-    result3 = Penyim(test3, Lang.UNK)
-    assert unidecode(result3.formats[0][Lang.HSR]) == unidecode("va")
+    result3 = Penyim(test3, PenyimFormats.UNK)
+    assert unidecode(result3.formats[0][PenyimFormats.HSR]) == unidecode("va")
 
 
 def test_ambiguous_results():
     # ɛɪn (SL) and -ein (GC) is more common
     # test1 = "ben"
-    # result1 = Penyim(test1, Lang.UNK)
-    # assert unidecode(result1.formats[0][Lang.SL]) == unidecode("bɛɪn")
-    # assert unidecode(result1.formats[0][Lang.GC]) == unidecode("bein")
+    # result1 = Penyim(test1, PenyimFormats.UNK)
+    # assert unidecode(result1.formats[0][PenyimFormats.SL]) == unidecode("bɛɪn")
+    # assert unidecode(result1.formats[0][PenyimFormats.GC]) == unidecode("bein")
 
     # -ɛt (SL) and -eik (GC) are more common
     test2 = "set"
-    result2 = Penyim(test2, Lang.UNK)
-    assert unidecode(result2.formats[0][Lang.SL]) == unidecode("sɛt")
-    assert unidecode(result2.formats[0][Lang.GC]) == unidecode("seik")
+    result2 = Penyim(test2, PenyimFormats.UNK)
+    assert unidecode(result2.formats[0][PenyimFormats.SL]) == unidecode("sɛt")
+    assert unidecode(result2.formats[0][PenyimFormats.GC]) == unidecode("seik")
 
 
 def test_labial_onglide_inclusion():
     test1 = "muo"
-    result1 = Penyim(test1, Lang.UNK)
-    assert unidecode(result1.formats[0][Lang.HSR]) == unidecode("mo")
+    result1 = Penyim(test1, PenyimFormats.UNK)
+    assert unidecode(result1.formats[0][PenyimFormats.HSR]) == unidecode("mo")
 
     test2 = "nguoi"
-    result2 = Penyim(test2, Lang.UNK)
-    assert unidecode(result2.formats[0][Lang.HSR]) == unidecode("ngoi")
+    result2 = Penyim(test2, PenyimFormats.UNK)
+    assert unidecode(result2.formats[0][PenyimFormats.HSR]) == unidecode("ngoi")
 
     test3 = "uon"
-    result3 = Penyim(test3, Lang.UNK)
-    assert unidecode(result3.formats[0][Lang.HSR]) == unidecode("on")
+    result3 = Penyim(test3, PenyimFormats.UNK)
+    assert unidecode(result3.formats[0][PenyimFormats.HSR]) == unidecode("on")
 
     test4 = "huot"
-    result4 = Penyim(test4, Lang.UNK)
-    assert unidecode(result4.formats[0][Lang.HSR]) == unidecode("hot")
+    result4 = Penyim(test4, PenyimFormats.UNK)
+    assert unidecode(result4.formats[0][PenyimFormats.HSR]) == unidecode("hot")
 
 
 # def test_multiple_romanizations():
 #     test = "ngi sib xei"
-#     result = Penyim(test, Lang.UNK)
+#     result = Penyim(test, PenyimFormats.UNK)
 #     expected_result = ["ng(e)i", "sip", "lh(e)i"]
 #     for i, word_formats in enumerate(result.formats):
-#         assert unidecode(word_formats[Lang.HSR]) == unidecode(expected_result[i])
+#         assert unidecode(word_formats[PenyimFormats.HSR]) == unidecode(expected_result[i])
 
 
 def test_missing_final_tone():
     test = "fān-uï"
-    result = Penyim(test, Lang.UNK)
-    assert unidecode(result.formats[0][Lang.HSR]) == unidecode("fan55")
-    assert unidecode(result.formats[1][Lang.HSR]) == unidecode("ui33")
+    result = Penyim(test, PenyimFormats.UNK)
+    assert unidecode(result.formats[0][PenyimFormats.HSR]) == unidecode("fan55")
+    assert unidecode(result.formats[1][PenyimFormats.HSR]) == unidecode("ui33")
 
 
 def test_nan_segment():
     test = "nãn-häm"
-    result = Penyim(test, Lang.GC)
-    assert unidecode(result.formats[0][Lang.GC]) == unidecode("nãn")
-    assert unidecode(result.formats[1][Lang.GC]) == unidecode("häm")
+    result = Penyim(test, PenyimFormats.GC)
+    assert unidecode(result.formats[0][PenyimFormats.GC]) == unidecode("nãn")
+    assert unidecode(result.formats[1][PenyimFormats.GC]) == unidecode("häm")
 
 
 def test_sl_apple():
     test = "pɛɪn22 gɔ55"
-    result = Penyim(test, Lang.SL)
-    assert unidecode(result.formats[0][Lang.SL]) == unidecode("pɛɪn22")
-    assert unidecode(result.formats[1][Lang.SL]) == unidecode("gɔ55")
+    result = Penyim(test, PenyimFormats.SL)
+    assert unidecode(result.formats[0][PenyimFormats.SL]) == unidecode("pɛɪn22")
+    assert unidecode(result.formats[1][PenyimFormats.SL]) == unidecode("gɔ55")
